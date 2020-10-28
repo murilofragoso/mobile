afe75328294c989ebca4fbf115d8a316658b4c4f
Murilo Fragoso <mu_fragoso@hotmail.com>
2020-10-28 20:18:20 -0300

    Dia 2 de estudo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,48 @@
 import React from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
 import { View, ImageBackground, Text, Image, StyleSheet } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
-    return (
-        <ImageBackground 
-            source={require('../../assets/home-background.png')} 
-            style={styles.container}
-            imageStyle={{ width: 274, height: 368 }}
-        >
-            <View style={styles.main}>
-                <Image source={require('../../assets/logo.png')} />
-                <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-                <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
-            </View>
+  const navigation = useNavigation();
 
-            <View style={styles.footer}>
+  function handleNavigateToPoints() {
+    navigation.navigate('Points');
+  }
 
-            </View>
-        </ImageBackground>
-    );
+  return (
+    <ImageBackground 
+        source={require('../../assets/home-background.png')} 
+        style={styles.container}
+        imageStyle={{ width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')} />
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} ></Icon>
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
+  );
 };
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      padding: 32,
-      backgroundColor: '#f0f0f5'
+      padding: 32      
     },
   
     main: {
